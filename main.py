c3b13fc7cee996b2d12da02104adb4c6fd8b7e6d
kimonanagnostopoulos <anagnostopouloskimon@gmail.com>
2024-11-11 01:56:19 -0800

    main.py basic

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-# Main entry point for the Pico W
-# Import and call function from the IMU and network modules
+from imu.imu_handler import initialize_imu, read_imu_data
+from network.wifi_connect import connect_wifi
+from network.tcp_client import connect_server, send_data
 
+def main():
+    return
 
+if __name__ == "__main__":
+    main()
